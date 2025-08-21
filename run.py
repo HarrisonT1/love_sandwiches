c0191e74b846cb88e82115a8a02727713a010395
HarrisonT1 <harrisontownend@hotmail.com>
2025-08-21 21:18:46 +0100

    Adju8st validation to check that all values provided at integers

--- run.py
+++ run.py
@@ -2,10 +2,10 @@ import gspread
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
+	"https://www.googleapis.com/auth/spreadsheets",
+	"https://www.googleapis.com/auth/drive.file",
+	"https://www.googleapis.com/auth/drive"
+	]
 
 CREDS = Credentials.from_service_account_file('creds.json')
 SCOPED_CREDS = CREDS.with_scopes(SCOPE)
@@ -21,6 +21,25 @@ def get_sales_data():
 	print("Example: 10,20,30,40,50,60\n")
 
 	data_str = input("Enter your data here: ")
-	print(f"the data provided is {data_str}")
+
+	sales_data = data_str.split(",")
+
+	validate_data(sales_data)
+
+def validate_data(values):
+	"""
+	inside the try, converts all string values into integers.
+	raises ValueError if strings cannot be converted into int
+	or if there arn't exactly 6 values
+	"""
+	try:
+		[int(value) for value in values] 
+		if len(values) != 6:
+			raise ValueError(
+				f"Exactly 6 values are required, you provided {len(values)}"
+			)
+	except ValueError as e:
+		print(f"Invalid data: {e}, please try again. \n")
+
 
 get_sales_data()
